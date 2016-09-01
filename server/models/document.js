e67de18b0a2334c5a9b2a2724123a0370463aeb8
Njeri Kieha <sylvia.kieha@andela.com>
2016-09-01 11:37:08 +0300

    Document model

---
+++ server/models/document.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const DocumentSchema = new Schema({
+  title: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+
+  content: {
+    type: String,
+    required: true,
+  },
+
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+
+  access: {
+    type: Schema.Types.ObjectId,
+    ref: 'Role',
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Document', DocumentSchema);
