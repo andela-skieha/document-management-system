1f01ab8d20e803d5f55e4d499ed3d7d406b612a6
Njeri Kieha <sylvia.kieha@andela.com>
2016-09-02 16:44:05 +0300

    Using dotenv package for environment variables

--- server/config.js
+++ server/config.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
   port: process.env.PORT || 5000,
-  database: 'mongodb://localhost/dms',
+  database: process.env.MONGO_PROD_URL,
+  test_database: process.env.MONGO_TEST_URL,
   secret: 'this is super secret',
 };
