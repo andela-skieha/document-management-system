ab2e1fc5ee27848be164a5a9b75b8c2f0294a211
Njeri Kieha <sylvia.kieha@andela.com>
2016-09-13 17:24:33 +0300

    Code refactor

--- server/config.js
+++ server/config.js
@@ -4,5 +4,5 @@ module.exports = {
   port: process.env.PORT || 5000,
   database: process.env.MONGO_PROD_URL,
   test_database: process.env.MONGO_TEST_URL,
-  secret: 'this is super secret',
+  secret: process.env.MONGO_SECRET,
 };
