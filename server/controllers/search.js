78baf14b9716d3f1a70d823f21efb95a2edb0980
Njeri Kieha <sylvia.kieha@andela.com>
2016-09-15 12:04:31 +0300

    Search by role

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-underscore-dangle */
+
 const Document = require('../models/document');
+const Role = require('../models/role');
 
 module.exports = {
   search: (req, res) => {
@@ -20,6 +23,29 @@ module.exports = {
           res.status(404).send({ error: `No documents created on ${req.query.date} were found.` });
         } else {
           res.status(200).send(documents);
+        }
+      });
+    } else if (req.query.role) {
+      Role.findOne({
+        title: req.query.role,
+      })
+      .exec((err, role) => {
+        if (err || role === null) {
+          res.status(404).send({ error: `Role ${req.query.role} does not exist.` });
+        } else {
+          Document.find({
+            role: role._id,
+          })
+          .limit(parseInt(req.query.limit, 10))
+          .sort({ createdAt: -1 })
+          .populate('owner role', 'username title -_id')
+          .exec((error, documents) => {
+            if (error || documents.length === 0) {
+              res.status(404).send({ error: `Documents with role ${req.query.role} not found.` });
+            } else {
+              res.status(200).send(documents);
+            }
+          });
         }
       });
     } else {
