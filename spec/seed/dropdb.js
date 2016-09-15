14e1e92c7466e7c58a5c731958272947040e7404
Njeri Kieha <sylvia.kieha@andela.com>
2016-09-15 16:19:28 +0300

    Role tests

--- spec/seed/dropdb.js
+++ spec/seed/dropdb.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const config = require('../../server/config');
 const User = require('../../server/models/user');
 const Document = require('../../server/models/document');
+const Role = require('../../server/models/role');
 
 mongoose.connect(config.test_database, (err) => {
   if (err) console.error('Mongoose error: ', err);
@@ -23,6 +24,14 @@ mongoose.connection.on('connected', () => {
       console.error(err);
     } else {
       console.log('Successfully removed documents');
+    }
+  });
+
+  Role.remove({}, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Successfully removed roles');
     }
     process.exit();
   });
