891d6104f4e77a0352a94344fe2df80faa6ca8c0
Njeri Kieha <sylvia.kieha@andela.com>
2016-09-16 12:08:02 +0300

    Find and update role by id

--- server/controllers/roles.js
+++ server/controllers/roles.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
 
 const Role = require('../models/role');
 
@@ -39,6 +40,59 @@ module.exports = {
       } else {
         res.status(200).send(roles);
       }
+    });
+  },
+
+  findOne: (req, res) => {
+    Role
+    .findById(req.params.id)
+    .populate('owner members', 'username -_id')
+    .exec((err, role) => {
+      if (err || role === null) {
+        res.status(404).send({ error: 'Role not found.' });
+      } else {
+        res.status(200).send(role);
+      }
+    });
+  },
+
+  updateOne: (req, res) => {
+    Role.findById(req.params.id, (err, role) => {
+      if (err || role === null) {
+        res.status(404).send({ error: 'Role not found.' });
+        return;
+      }
+
+      if (req.body.title) role.title = req.body.title;
+
+      if (req.body.addMembers) {
+        req.body.addMembers.forEach((member) => {
+          if (role.members.indexOf(member) === -1) {
+            role.members.push(member);
+          }
+        });
+      }
+
+      if (req.body.removeMembers) {
+        req.body.removeMembers.forEach((member) => {
+          if (role.members.indexOf(member) !== -1) {
+            role.members.splice(role.members.indexOf(member), 1);
+          }
+        });
+      }
+
+      role.save((error) => {
+        if (error) {
+          if (error.code === 11000) {
+            res.status(409).send({ error: 'Duplicate entry.' });
+            return;
+          }
+        } else if (Object.keys(req.body).length === 0) {
+          res.status(400).send({ error: 'Nothing to update.' });
+        } else {
+          res.status(200).send({ message: 'Role updated successfully.' });
+        }
+      });
     });
   },
 };
--- server/routes/roles.js
+++ server/routes/roles.js
@@ -4,4 +4,8 @@ module.exports = (router) => {
   router.route('/roles')
     .post(Roles.create)
     .get(Roles.all);
+
+  router.route('/roles/:id')
+    .get(Roles.findOne)
+    .put(Roles.updateOne);
 };
