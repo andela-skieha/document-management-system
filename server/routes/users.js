1c28da0cc86479244d6a8b48a28f53e72ed29d33
Njeri Kieha <sylvia.kieha@andela.com>
2016-09-20 12:50:14 +0300

    Code refactor

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,6 @@ const Users = require('../controllers/users');
 
 module.exports = (router) => {
   router.route('/users')
-    .post(Users.create)
     .get(Users.all);
 
   router.route('/users/:user_id')
